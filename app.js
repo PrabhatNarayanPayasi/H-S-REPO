4971f18d283ac0fc67739dd92c75b17fb5830c33
Prabhat Narayan PAyasi <prabhatnarayanpayasi@gmail.com>
2025-03-25 23:02:23 +0530

    H2S Code

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const path = require('path');
 const app = express();
 const port = 3000;
  
+ 
+app.set('view engine', 'ejs');  // Set the EJS view engine
+
 app.use(express.static(path.join(__dirname, 'public')));
- 
-app.set('view engine', 'ejs');
- 
+
 app.get('/', (req, res) => {
-  res.render("Home.ejs");
+  res.render('Home.ejs'); // Render your ejs views normally
 });
 
 
@@ -16,10 +17,17 @@ app.get("/Dashboard" , (req , res)=>{
   res.render("Dashboard.ejs")
 })
 
-
-app.get("/setting" , (req,res)=>{
-  res.render("setting.ejs")
+app.get("/inventory" , (req , res)=>{
+  res.render("inventory.ejs")
 })
+
+app.get("/Integration" , (req , res)=>{
+  res.render("Integration.ejs")
+})
+app.get('/setting', (req, res) => {
+  res.render('setting.ejs'); // Render the setting.ejs view
+});
+
 app.listen(port, () => {
   console.log(`Server chal raha hai http://localhost:${port}`);
 });
